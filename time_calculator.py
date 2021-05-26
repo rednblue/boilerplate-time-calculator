f83f10611d267b1f0df5961c8057db048b7c69f4
Mauro Leonelli <mleonelli@gmail.com>
2021-05-26 07:08:40 +0000

    Initial commit of version 0.1 of the solution

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,78 @@
-def add_time(start, duration):
+# Version 0.1 of add_time function
+# This version simply satisfies the unit tests without any improvement to code performance. 
+# I might come back to tackle some of the shortcuts taken to make it work
+
+def add_time(start, duration, *args):
+
+  new_time = None
+  days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+  hourPart = start.index(":")
+  minutePart = start.index(" ")
+  
+  sHour = int(start[:hourPart])
+  sMinute = int(start[hourPart+1:minutePart])
+  sAMPM = start[minutePart+1:]
+
+  hourPart = duration.index(":")
+  dHour = int(duration[:hourPart])
+  dMinute = int(duration[hourPart+1:])
+
+  day = None
+  for argument in args:
+    day = argument.lower()
+
+  dint = 0
+  for d in days:
+    if d == day:
+      break
+    dint += 1
+
+  nHour = sHour
+  nMinute = sMinute
+
+  if(sAMPM == "PM"):
+    nHour += 12
+
+  nMinute = sMinute + dMinute
+  if(nMinute > 60):
+    nMinute = nMinute - 60
+    nHour += 1
+
+  tHour = nHour + dHour
+  nHour = tHour % 24
+  
+  dayAfter = int(tHour / 24)
 
 
 
+  nAMPM = "AM"
+  if(nHour > 12):
+    nAMPM = "PM"
+    nHour = nHour - 12
 
+  nMinuteStr = str(nMinute)
+  if(nMinute < 10):
+    nMinuteStr = "0" + nMinuteStr
 
-    return new_time
+  nHourStr = str(nHour)
+  
+  if(nHour == 0 and nAMPM == "AM"):
+    nHourStr = "12"
+
+  if(nHour == 12 and nAMPM == "AM"):
+    nAMPM = "PM"
+
+  new_time = nHourStr + ":" + nMinuteStr + " " + nAMPM  
+
+  if(day is not None):
+    dint = dint + dayAfter
+    d = days[dint%7]
+    new_time += ", " + d.title()
+
+  if(dayAfter == 1):
+    new_time += " (next day)"
+  elif(dayAfter > 1):
+    new_time += " (" + str(dayAfter) + " days later)"
+
+  return new_time
